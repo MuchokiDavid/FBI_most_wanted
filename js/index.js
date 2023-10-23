2e1f43c58bd47f0dc6ec11270de149e229171f0c
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-10-23 22:44:11 +0300

    side menu implemeted

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
     // function to fetch data
 async function fetchData(){
     fetch('https://api.fbi.gov/wanted/v1/list')
-  .then(response => response.json())
-   .then(data =>{
+    .then(response => response.json())
+    .then(data =>{
         console.log(data.items)
         const fugitiveList = document.querySelector("#items")
         for(let index=0; index<data.items.length; index++){
@@ -14,24 +14,39 @@ async function fetchData(){
             <div class="ml-1 my-2 col-4">
                 <div class="card" >
                     <img src="${data.items[index].images[0].original}" height="250px" class="card-img-top image" alt="...">
-                    
                     <div id= "card-body" class="card-body" style = "color: black; text-align: left;">
-                        <h5 class="card-title">Name: ${data.items[index].title}</h5>
+                        <h5 class="card-title">NAME: ${data.items[index].title}</h5>
                         <ul style = "list-style: none">
                             <li>Place of birth: ${data.items[index].place_of_birth}</li>
                             <li>Publication: ${data.items[index].publication}</li>
                         </ul>
-                        <button class="btn btn-sm btn-primary">View more</button>
+                        <button class= "viewMore" >View more</button>
                     </div>
                 </div>
             </div>
             `
         }
-   })
-  .catch(error =>error);
+    })
+    .catch(error =>error);
 }
 fetchData()
 
+async function fetchCriminals(){
+        fetch('https://api.fbi.gov/wanted/v1/list')
+        .then(response => response.json())
+        .then(data =>{
+        const ul = document.querySelector("#menuUl")
+        for (let index = 0; index < data.items.length; index++) {
+            const list = document.createElement("li")
+            list.textContent= data.items[index].title;
+            list.className = "list"
+            list.addEventListener("click", ()=>{
+                
+            })
+            ul.appendChild(list)
+        }
+    })
+}
 
-
+fetchCriminals()
 })
