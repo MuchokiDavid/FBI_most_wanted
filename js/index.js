52d33bf450d6ee046c86c1a7c61987aa74a52e01
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-10-24 12:47:39 +0300

    Added list events

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-//array of items
-let persons = [];
-
+// document listener to preload the DOM before loading the scripts
 document.addEventListener("DOMContentLoaded", ()=>{
     // function to fetch data
 async function fetchData(){
@@ -16,11 +14,10 @@ async function fetchData(){
                     <img src="${data.items[index].images[0].original}" height="250px" class="card-img-top image" alt="...">
                     <div id= "card-body" class="card-body" style = "color: black; text-align: left;">
                         <h5 class="card-title">NAME: ${data.items[index].title}</h5>
-                        <ul style = "list-style: none">
-                            <li>Place of birth: ${data.items[index].place_of_birth}</li>
-                            <li>Publication: ${data.items[index].publication}</li>
+                        <ul style = "list-style: none; text-align: left">
+                            <li>Publication time: ${data.items[index].publication}</li>
                         </ul>
-                        <button class= "viewMore" >View more</button>
+                        <button class= "viewMore" >More details</button>
                     </div>
                 </div>
             </div>
@@ -31,22 +28,67 @@ async function fetchData(){
 }
 fetchData()
 
+//function to fetch data and put name on a list menu
 async function fetchCriminals(){
         fetch('https://api.fbi.gov/wanted/v1/list')
         .then(response => response.json())
         .then(data =>{
         const ul = document.querySelector("#menuUl")
         for (let index = 0; index < data.items.length; index++) {
+            const listDiv = document.createElement("div")
+            listDiv.className = "listDiv"
+            ul.appendChild(listDiv)
             const list = document.createElement("li")
             list.textContent= data.items[index].title;
             list.className = "list"
-            list.addEventListener("click", ()=>{
+            listDiv.addEventListener("click", ()=>{
+                const outerCard= document.querySelector("#outerDiv")
+                //create card to display criminals details(
+                const outerDiv= document.querySelector("#outerouterdiv")
+                //remove division holding all lists
+                if (outerCard) {
+                    outerCard.style.display = 'none'; // remove outer card to display individual card
+                }
+                //remove existing card of each item on the list
+                const existingCard = outerDiv.querySelector(".detailsDiv")
+                if(existingCard){
+                    outerDiv.removeChild(existingCard)
+                }
+                const detailsDiv= document.createElement("div")
+                detailsDiv.className = "detailsDiv"
+                detailsDiv.innerHTML= `
+                    <div class="card" style= "width: 100%">
+                        <img src="${data.items[index].images[0].original}" height="250px" class="card-img-top image" alt="...">
+                        <div id= "card-body" class="card-body" style = "color: black; text-align: left;">
+                            <h5 class="card-title">NAME: ${data.items[index].title}</h5>
+                            <p>Subject: ${data.items[index].subjects[0]}</p>
+                            <p>Publication: ${data.items[index].publication}</p><br>
+
+                            <h6>Suspect Information</h6>
+                            <ul style = "list-style: none">
+                                <li>Date of birth: ${data.items[index].dates_of_birth_used}</li>
+                                <li>Place of birth: ${data.items[index].place_of_birth}</li>
+                                <li>Gender: ${data.items[index].sex}</li> 
+                                <li>Race: ${data.items[index].race}</li>
+                                <li>Hair Color: ${data.items[index].hair}</li>
+                                <li>Eyes Color: ${data.items[index].eyes}</li>
+                                <li>Description: ${data.items[index].description}</li>
+                                <li>Remarks: ${data.items[index].remarks}</li>
+                                </ul>
+                                
+                            <a href="index.html"><button class= "viewMore" >Back Home</button></a>
+                        </div>
+                    </div>
+                `
+                outerDiv.appendChild(detailsDiv)
+                
                 
             })
-            ul.appendChild(list)
+            listDiv.appendChild(list)
         }
     })
 }
-
 fetchCriminals()
+
+//function to add event listener to button
 })
